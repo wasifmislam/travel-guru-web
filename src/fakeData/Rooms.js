16c4bb28704315a94dece94d7d292d0ffe43cc77
wasifmislam <67533737+wasifmislam@users.noreply.github.com>
2020-09-22 15:03:16 +0600

    travel-guru-web is coming

---
+++ src/fakeData/Rooms.js
@@ -0,0 +1,114 @@
+var hotelRooms = [
+    {
+        name: 'Apartment in lost Sajek',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi Air conditioning',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 50,
+        rating:4.8,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/C1FvFDh/Rectangle-26.png',
+        hotelType: 'sajek'
+
+    },
+    {
+        name: 'Classic Resourt',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 40,
+        rating:4.7,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/fkPchRB/Rectangle-27.png',
+        hotelType: 'sajek'
+
+    },
+    {
+        name: 'Eco Cottage in Sajek',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 60,
+        rating:4.4,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/ZXywCRL/Rectangle-28.png',
+        hotelType: 'sajek'
+
+    },
+    {
+        name: 'Hotel Sreemongol',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 50,
+        rating:4.3,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/fkPchRB/Rectangle-27.png',
+        hotelType: 'sreemongol'
+
+    },
+    {
+        name: 'Sreemongol Palace',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 40,
+        rating:4.9,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/C1FvFDh/Rectangle-26.png',
+        hotelType: 'sreemongol'
+
+    },
+    {
+        name: 'Hotel Paradise',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 60,
+        rating:4.6,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/ZXywCRL/Rectangle-28.png',
+        hotelType: 'sreemongol'
+
+    },
+    {
+        name: 'Apartment in lost the lost S',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 50,
+        rating:4.4,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/ZXywCRL/Rectangle-28.png',
+        hotelType: 'sundorbon'
+
+    },
+    {
+        name: 'Forest Resourt',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 40,
+        rating:4.7,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/C1FvFDh/Rectangle-26.png',
+        hotelType: 'sundorbon'
+
+    },
+    {
+        name: 'Eco Cottage of Sundorbon',
+        // Instructor: 'Stephen Rivon',
+        wifi: 'WiFi conditioning kitchen',
+        flexibility: 'Cancellation flexibiliti is available',
+        rate: 60,
+        rating:4.8,
+        //students:`750K student's`,
+        image: 'https://i.ibb.co/fkPchRB/Rectangle-27.png',
+        hotelType: 'sundorbon'
+
+    }
+]
+
+export default hotelRooms;
+
+
